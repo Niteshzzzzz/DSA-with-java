b4b354a8b932e0b679528fb7091014674426a9cd
Niteshzzzzz <niteshkumarnkzzzzz@gmail.com>
2025-03-02 01:19:13 +0530

    learning stack

---
+++ Stack in java/duplicate parentheses/DuplicateParentheses.java
@@ -0,0 +1,35 @@
+// duplicate parentheses means using parantheses in a expression without need
+// ex - (a+(b-c)) => false
+// ex - ((a+(b-c))) => true
+
+import java.util.Stack;
+
+public class DuplicateParentheses {
+
+    static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true;
+                } else{
+                    s.pop();
+                }
+            } else {
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        String str = "((a-(b+c)))";
+        System.out.println(isDuplicate(str));
+    }
+}
